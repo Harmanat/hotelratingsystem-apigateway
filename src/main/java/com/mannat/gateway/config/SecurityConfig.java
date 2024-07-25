7dab1d41903ab6719011119ac29fc7d3bf690461
HarmanatS <wish.mannat@gmail.com>
2024-07-25 12:19:58 +0530

    Implementing Spring Security in API Gateway with Okta

--- src/main/java/com/mannat/gateway/config/SecurityConfig.java
+++ src/main/java/com/mannat/gateway/config/SecurityConfig.java
@@ -2,26 +2,26 @@ package com.mannat.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-//import org.springframework.security.config.web.server.ServerHttpSecurity;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-//@EnableWebFluxSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 	
-//	@Bean
-//	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
-//		httpSecurity
-//			.authorizeExchange()
-//			.anyExchange()
-//			.authenticated()
-//			.and()
-//			.oauth2Login()
-//			.and()
-//			.oauth2ResourceServer()
-//			.jwt();
-//
-//		return httpSecurity.build();
-//	}
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+		httpSecurity
+			.authorizeExchange()
+			.anyExchange()
+			.authenticated()
+			.and()
+			.oauth2Login()
+			.and()
+			.oauth2ResourceServer()
+			.jwt();
+
+		return httpSecurity.build();
+	}
 }
